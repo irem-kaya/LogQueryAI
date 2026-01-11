049cd7b1bf0fa2ddfbdaa6f7f690ed6b9d39eb60
İrem Nur Kaya <116844116+irem-kaya@users.noreply.github.com>
2026-01-11 10:58:37 +0300

    Update data_preparation_and_preprocessing.py

--- data_preparation_and_preprocessing.py
+++ data_preparation_and_preprocessing.py
@@ -3,13 +3,12 @@ from sklearn.preprocessing import LabelEncoder
 import numpy as np
 import faiss
 
-# Load CSV data
+
 df = pd.read_csv("data/client_hostname.csv")
 
 # Select and clean necessary columns
 cleaned_data = df[['client', 'hostname', 'alias_list', 'address_list']].copy()
 
-# Encode categorical columns
 client_encoder = LabelEncoder()
 cleaned_data['client_encoded'] = client_encoder.fit_transform(cleaned_data['client'])
 
@@ -22,15 +21,13 @@ cleaned_data['alias_list_encoded'] = alias_list_encoder.fit_transform(cleaned_da
 address_list_encoder = LabelEncoder()
 cleaned_data['address_list_encoded'] = address_list_encoder.fit_transform(cleaned_data['address_list'].astype(str))
 
-# Combine encoded columns into a single array
 vectorized_data = cleaned_data[['client_encoded', 'hostname_encoded', 'alias_list_encoded', 'address_list_encoded']].values
 print("Vectorized Data Shape:", vectorized_data.shape)
 
-# Initialize FAISS index
 vector_dim = vectorized_data.shape[1]  # Update dimension based on encoded columns
 index = faiss.IndexFlatL2(vector_dim)
 
-# Convert to float32 and add to FAISS index
 vectorized_data = np.array(vectorized_data, dtype=np.float32)
 index.add(vectorized_data)
 print("Number of vectors in index:", index.ntotal)
+
